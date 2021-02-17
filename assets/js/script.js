6cef49f9494a66637460552911dac2d9472bd430
tJay11 <74987365+tJay11@users.noreply.github.com>
2021-02-17 14:59:50 +0000

    Piano with HTML,CSS,JavaScript

---
+++ assets/js/script.js
@@ -0,0 +1,241 @@
+const C4 = new Audio("assets/sounds/C4.mp3")
+function c4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(C4);
+}
+
+const Db4 = new Audio("assets/sounds/Db4.mp3");
+function db4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Db4);
+}
+
+const D4 = new Audio("assets/sounds/D4.mp3");
+function d4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(D4);
+}
+
+const Eb4 = new Audio("assets/sounds/Eb4.mp3");
+function eb4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Eb4);
+}
+
+const E4 = new Audio("assets/sounds/E4.mp3");
+function e4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(E4);
+}
+
+const F4 = new Audio("assets/sounds/F4.mp3");
+function f4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(F4);
+}
+
+const Gb4 = new Audio("assets/sounds/Gb4.mp3");
+function gb4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Gb4);
+}
+
+const G4 = new Audio("assets/sounds/G4.mp3");
+function g4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(G4);
+}
+
+const Ab4 = new Audio("assets/sounds/Ab4.mp3");
+function ab4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Ab4);
+}
+
+const A4 = new Audio("assets/sounds/A4.mp3");
+function a4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(A4);
+}
+
+const Bb4 = new Audio("assets/sounds/Bb4.mp3");
+function bb4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Bb4);
+}
+
+const B4 = new Audio("assets/sounds/B4.mp3");
+function b4Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(B4);
+}
+
+const C5 = new Audio("assets/sounds/C5.mp3");
+function c5Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(C5);
+}
+
+const Db5 = new Audio("assets/sounds/Db5.mp3");
+function db5Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Db5);
+}
+
+const D5 = new Audio("assets/sounds/D5.mp3");
+function d5Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(D5);
+}
+
+const Eb5 = new Audio("assets/sounds/Eb5.mp3");
+function eb5Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(Eb5);
+}
+
+const E5 = new Audio("assets/sounds/E5.mp3");
+function e5Key() {
+    const playSound = audio => {
+        const clone = audio.cloneNode();
+        clone.play();
+        setTimeout(() => (clone.volume = 0.8), 400);
+        setTimeout(() => (clone.volume = 0.6), 800);
+        setTimeout(() => (clone.volume = 0.4), 1200);
+        setTimeout(() => (clone.volume = 0.2), 1600);
+        setTimeout(() => (clone.volume = 0), 2000);
+    };
+    playSound(E5);
+}
+
+
+
+
